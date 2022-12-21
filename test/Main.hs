e977234123c30fd51f434da8267bdc64e2076063
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-21 19:13:51 +0000

    Added hunit test

--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,17 @@
 module Main (main) where
 
-
 import qualified HelloWorld
-
+import qualified Test.Tasty
+import qualified Test.Tasty.HUnit
 
 main :: IO ()
 main =
-    putStrLn (HelloWorld.hello "test")
+  Test.Tasty.defaultMain tests
+
+tests :: Test.Tasty.TestTree
+tests =
+  Test.Tasty.testGroup
+    "Tests"
+    [ Test.Tasty.HUnit.testCase "hello world" $
+        HelloWorld.hello "world" Test.Tasty.HUnit.@?= "hello world"
+    ]
